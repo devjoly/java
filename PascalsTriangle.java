9b2331b50df1ac95d7fde5aba9fe9554fda35c1c
Joly <joly97641@gmail.com>
2026-02-15 21:34:30 +0600

    PascalsTriangle program

---
+++ PascalsTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int s = 0; s < rows - i; s++) {
+                System.out.print(" ");
+            }
+
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            
+            System.out.println();
+        }
+        sc.close();
+    }
+}
